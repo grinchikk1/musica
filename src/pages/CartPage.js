412a618f122790ef1e5cc2772dc78cd7b5d2ae55
Dmytro Hrinchenko <demonchikk1@gmail.com>
2023-06-21 20:03:25 +0300

    update cart page

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -9,30 +9,21 @@ import { useDispatch } from "react-redux";
 
 const CartPage = ({ toggleFavorite, cart, deleteToCart, favorites }) => {
   const dispatch = useDispatch();
-
-  const handleDeleteAllFromCart = () => {
-    dispatch(deleteAllFromCart());
-  };
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const handleOpenModalForm = () => {
-    setIsModalOpen(true);
-  };
+  const handleOpenModalForm = () => setIsModalOpen(true);
+  const handleModalClose = () => setIsModalOpen(false);
 
-  const handleModalClose = () => {
-    setIsModalOpen(false);
-  };
-  const cartList = cart.map((card) => {
-    return (
-      <ProductCart
-        key={card.id}
-        toggleFavorite={toggleFavorite}
-        deleteToCart={deleteToCart}
-        isFavorite={favorites.some((p) => p.id === card.id)}
-        card={card}
-      />
-    );
-  });
+  const cartList = cart.map((card) => (
+    <ProductCart
+      key={card.id}
+      toggleFavorite={toggleFavorite}
+      deleteToCart={deleteToCart}
+      isFavorite={favorites.some((p) => p.id === card.id)}
+      card={card}
+    />
+  ));
+
   return (
     <div className="buy-now">
       {cart.length > 0 && (
@@ -45,14 +36,18 @@ const CartPage = ({ toggleFavorite, cart, deleteToCart, favorites }) => {
       {isModalOpen && (
         <ModalForm
           cart={cart}
-          deleteAllFromCart={handleDeleteAllFromCart}
+          deleteAllFromCart={() => dispatch(deleteAllFromCart())}
           closeButton={true}
           isOpen={isModalOpen}
           handleModalClose={handleModalClose}
           onRequestClose={handleModalClose}
         />
       )}
-      <ul className="cart-list">{cartList}</ul>
+      {cart.length > 0 ? (
+        <ul className="cart-list">{cartList}</ul>
+      ) : (
+        <p>Your cart is empty.</p>
+      )}
     </div>
   );
 };
